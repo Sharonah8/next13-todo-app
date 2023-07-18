182c0245bb92825a8cbd2852aa8603a028c054a5
Snyamongo <Snyamongo@safaricom.co.ke>
2023-07-18 08:21:34 +0300

    Style the form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -3,7 +3,7 @@ export default function Page(){
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">New</h1>
     </header>
-    <form action="">
+    <form className="flex gap-2 flex-col">
         <input type="text" name="title"
         className="border border-slate-300 bg-transparent rounded px-2 py-1
         outline-none focus-within:border-slate-100" />
