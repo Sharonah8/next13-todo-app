62b77e6f123f36ab2beae5b7c0b6742c7206f3f6
Snyamongo <Snyamongo@safaricom.co.ke>
2023-07-18 08:28:28 +0300

    Style the buttons

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -9,7 +9,7 @@ export default function Page(){
         <input type="text" name="title"
         className="border border-slate-300 bg-transparent rounded px-2 py-1
         outline-none focus-within:border-slate-100" />
-        <div>
+        <div className="flex gap-1 justify-end">
             <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded
       hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Cancel</Link>
             <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded
