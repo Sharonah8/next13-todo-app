06c30de4eb1a57ab4fe7363dcf41e2f775ebfef7
Snyamongo <Snyamongo@safaricom.co.ke>
2023-07-18 08:47:57 +0300

    Create async function createTodo to get data from the server

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+async function createTodo(data:FormData) {
+    "use server"
+    
+}
 export default function Page(){
     return <>
     <header className="flex justify-between items-center mb-4">
